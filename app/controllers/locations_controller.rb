b659b7350bbc74bb1feae0abb0184e8bed6aff99
Ben Braunstein <benzbraunstein@gmail.com>
2019-09-06 13:24:10 -0400

    added a post method for locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,4 +3,14 @@ class LocationsController < ApplicationController
         locations = Location.all 
         render :json => locations
     end
+
+    def create 
+        location = Location.create(strong_location_params, rating: 1)
+        render :json => location
+    end
+
+    private
+    def strong_location_params
+        params.require(:location).permit(:address, :capacity, :name, :price, :image)
+    end
 end
